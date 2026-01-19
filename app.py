7ca1b8983c2bd3cf4ebc89986c5d63b3fbd1afdf
sairaj237 <magdumsairaj21@gmail.com>
2026-01-19 22:22:05 +0530

    v-2

--- app.py
+++ app.py
@@ -2,47 +2,63 @@ import gradio as gr
 from PIL import Image
 import torchvision.transforms as transforms
 import torch
-from cnn_model import PneumoniaCNN
 import numpy as np
 from gradcam import GradCAM
 import matplotlib.pyplot as plt
 import cv2
+from torchvision import models
+import torch.nn as nn
 
-
-model = PneumoniaCNN()
-model.load_state_dict(torch.load("pneumonia_cnn.pt", map_location="cpu"))
+model = models.resnet18(pretrained=False)
+model.fc = nn.Linear(model.fc.in_features, 3)
+model.load_state_dict(torch.load("pneumonia_resnet18_3class.pt", map_location="cpu"))
 model.eval()
 
-target_layer = model.conv2  # Choose the last conv layer
+target_layer = model.layer4
 gradcam = GradCAM(model, target_layer)
 
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225]
+    )
+])
 
 def predict(image):
 
-    # Convert NumPy array (from Gradio) to PIL Image
     if isinstance(image, np.ndarray):
         image = Image.fromarray(image)
 
-
-    transform = transforms.Compose([
-        transforms.Resize((150,150)),
-        transforms.ToTensor()
-    ])
     img_tensor = transform(image).unsqueeze(0)
+
     with torch.no_grad():
         output = model(img_tensor)
-        prob = output.item()
-    label = "Pneumonia" if prob > 0.5 else "Normal"
+        pred_class = torch.argmax(output, dim=1).item()
 
-    # GradCAM heatmap
+    label_map = {
+        0: "Normal",
+        1: "Pneumonia (Bacterial)",
+        2: "Pneumonia (Viral)"
+    }
+    label = label_map[pred_class]
+
+    # GradCAM
     cam = gradcam.generate(img_tensor)
-
-
+    cam = cv2.resize(cam, (224, 224))
     heatmap = cv2.applyColorMap(np.uint8(255 * cam), cv2.COLORMAP_JET)
-    img_np = np.array(image.resize((150,150)))
+    img_np = cv2.cvtColor(
+        np.array(image.resize((224, 224))),
+        cv2.COLOR_RGB2BGR
+            )
+
     overlay = cv2.addWeighted(img_np, 0.5, heatmap, 0.5, 0)
+    overlay = cv2.cvtColor(overlay, cv2.COLOR_BGR2RGB)
+
 
     return label, Image.fromarray(overlay)
+
 gr.Interface(
     fn=predict,
     inputs="image",
